4bc81f40a3bcac1f0b8d2d13bb74982d05e187bc
Vinodh kumar kurra <CIPL0707@colanonline.local>
2020-02-17 18:17:08 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
-
+import {connect} from "react-redux";
+import {service} from "./services/common/action"
 class App extends React.Component{
+
+
+    componentDidMount(){ 
+        let data = {
+            name : "maniiii",
+            age:"24"
+        }
+        this.props.anyother()
+    }
     render(){
+        console.log("mmmmmmmmmmmmmmmmmmmmmmmmm", this.props.userDetails)
         return(
             <div>
                 Hi I AM REACT AND REDUXZ BOILER PLATE
             </div>
         )
     }
+    }
+
+
+const mapActions =(dispatch) =>{
+    return{
+    anyother : () => dispatch(service())
+    }
 }
 
-export default App;
+const mapProps  = (state) =>{
+    return{
+        userDetails: state.commonReducer.userDetails
+    }
+}
+
+
+export default connect(mapProps,mapActions) (App);
